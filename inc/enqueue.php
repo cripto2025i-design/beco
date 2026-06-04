8d7fcda2c0c59380b09c3244ad7723bf13ad9bd3
cripto <cripto2025i@gmail.com>
2026-06-04 21:41:57 +0200

    style faq

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -24,6 +24,7 @@ if ( ! function_exists( 'understrap_scripts' ) ) {
 		wp_enqueue_style( 'understrap-theme', get_template_directory_uri() . '/css/theme.min.css', array(), $css_version );
 		wp_enqueue_style( 'understrap-styles', get_template_directory_uri() . '/dist/css/style.min.css', array(), $css_version );
         wp_enqueue_style( 'theme-plugins', get_template_directory_uri() . '/css/plugins.css' );
+		wp_enqueue_style( 'theme-styles', get_template_directory_uri() . '/style.css', array(), $css_version );
 
 		wp_enqueue_script( 'jquery' );
 
--- js/main.js
+++ js/main.js
@@ -9,5 +9,14 @@ jQuery(document).on('ready', function($) {
     });
 
     
+ /**
+   * Frequently Asked Questions Toggle
+   */
+  document.querySelectorAll('.faq-item h3, .faq-item .faq-toggle, .faq-item .faq-header').forEach((faqItem) => {
+    faqItem.addEventListener('click', () => {
+      faqItem.parentNode.classList.toggle('faq-active');
+    });
+  });
 
+  
 }(jQuery)); // End jQuery
